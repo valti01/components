c51da404651116ba9bf9abc0f715a2c8bb316271
Gila Valter <valti0190@gmail.com>
2025-11-18 10:05:14 +0100

    updated scripts

--- torch_models/run_eval.py
+++ torch_models/run_eval.py
@@ -12,97 +12,21 @@ def run_command(cmd):
         print(f"Error:\n{result.stderr}")
 
 
-targetlabels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'all', 'own']
-
+targetlabels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "all", "own", "random", "second"]
+#    "Linf": [0.09411764705, 0.18823529411, 0.37647058823, 0.56470588235, 0.75294117647]
 distance_norms = {
-    "L2": [1, 2, 3, 4, 5, 6],
+    "L2": [1.25, 2, 3, 4, 5, 6],
     "Linf": [0.09411764705, 0.18823529411, 0.37647058823, 0.56470588235, 0.75294117647]
 }
 
-model_names = ["hat", "ratio"]
+model_names = ["ramp","ratio"]
 
 for model_name in model_names:
     if model_name == "ratio":
-        dist_norm = "Linf"
-        distances = distance_norms[dist_norm]
-        for dist in distances:
-            for label in targetlabels:
-                cmd_global = [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--global_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv"
-                    "--fname",'ratio_05.pth'
-                ]
-                run_command(cmd_global)
-                cmd_adaptive =  [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--adaptive_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv",
-                    "--fname",'ratio_05.pth'
-                ]
-                run_command(cmd_adaptive)
-                cmd_global = [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--global_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv",
-                    "--fname",'ratio_025.pth'
-                ]
-                run_command(cmd_global)
-                cmd_adaptive =  [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--adaptive_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv"
-                    "--fname",'ratio_025.pth'
-                ]
-                run_command(cmd_adaptive)
-    elif model_name == "hat":
-        dist_norm = "L2"
-        distances = distance_norms[dist_norm]
-        for dist in distances:
-            for label in targetlabels:
-                cmd_global = [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--global_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv"
-                ]
-                run_command(cmd_global)
-                cmd_adaptive =  [
-                    sys.executable,
-                    "eval_auc.py",
-                    "--model_name", model_name,
-                    "--distance_norm", dist_norm,
-                    "--adaptive_min_distance", str(dist),
-                    "--targetlabel", str(label),
-                    "--out_fname", "bigmeasurement_results.csv"
-                ]
-                run_command(cmd_adaptive)
-    elif model_name == "ramp":
-        for dist_norm in ["L2", "Linf"]:
+        for dist_norm in ["Linf"]:
             distances = distance_norms[dist_norm]
             for dist in distances:
                 for label in targetlabels:
-                    if dist_norm == "L2":
-                        continue
                     cmd_global = [
                         sys.executable,
                         "eval_auc.py",
@@ -110,10 +34,10 @@ for model_name in model_names:
                         "--distance_norm", dist_norm,
                         "--global_min_distance", str(dist),
                         "--targetlabel", str(label),
-                        "--out_fname", "bigmeasurement_results_linf.csv"
+                        "--out_fname", "2025_11_results.csv",
+                        "--fname", 'ratio_05.pth'
                     ]
                     run_command(cmd_global)
-                    # Run with adaptive_min_distance
                     cmd_adaptive = [
                         sys.executable,
                         "eval_auc.py",
@@ -121,6 +45,104 @@ for model_name in model_names:
                         "--distance_norm", dist_norm,
                         "--adaptive_min_distance", str(dist),
                         "--targetlabel", str(label),
-                        "--out_fname", "bigmeasurement_results_linf.csv"
+                        "--out_fname", "2025_11_results.csv",
+                        "--fname", 'ratio_05.pth'
                     ]
                     run_command(cmd_adaptive)
+                    cmd_global = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--global_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv",
+                        "--fname", 'ratio_025.pth'
+                    ]
+                    run_command(cmd_global)
+                    cmd_adaptive = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--adaptive_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv",
+                        "--fname", 'ratio_025.pth'
+                    ]
+                    run_command(cmd_adaptive)
+    elif model_name == "hat":
+        for dist_norm in ["Linf"]:
+            distances = distance_norms[dist_norm]
+            for dist in distances:
+                for label in targetlabels:
+                    cmd_global = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--global_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv"
+                    ]
+                    #run_command(cmd_global)
+
+                    cmd_adaptive = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--adaptive_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv"
+                    ]
+                    run_command(cmd_adaptive)
+    elif model_name == "ramp":
+        for dist_norm in ["Linf"]:
+            distances = distance_norms[dist_norm]
+            for dist in distances:
+                for label in targetlabels:
+                    cmd_global = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--global_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv"
+                    ]
+                    #run_command(cmd_global)
+
+                    cmd_adaptive = [
+                        sys.executable,
+                        "eval_auc.py",
+                        "--model_name", model_name,
+                        "--distance_norm", dist_norm,
+                        "--adaptive_min_distance", str(dist),
+                        "--targetlabel", str(label),
+                        "--out_fname", "2025_11_results.csv",
+                        "--imgdir", "linf_bug_ramp"
+                    ]
+                    run_command(cmd_adaptive)
+    elif model_name == "atd":
+        for dist_norm in ["Linf"]:
+            distances = distance_norms[dist_norm]
+            for dist in distances:
+                for label in targetlabels:
+                    cmd_adaptive = [
+                        sys.executable,
+                        "atd/test_ATD.py",
+                        "--distance_norm", dist_norm,
+                        "--adaptive_min_distance", str(dist),
+                        "--targetlabel", str(label)
+                    ]
+                    run_command(cmd_adaptive)
+
+                    cmd_global = [
+                        sys.executable,
+                        "atd/test_ATD.py",
+                        "--distance_norm", dist_norm,
+                        "--global_min_distance", str(dist),
+                        "--targetlabel", str(label)
+                    ]
+                    run_command(cmd_global)
